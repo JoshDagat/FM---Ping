20a4c1c5021599fad082b997f53305db725753de
JosuerBague <83200271+JosuerBague@users.noreply.github.com>
2021-11-16 14:19:25 +0800

    Finished with design. Working on README.

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.form');
+const errorMsg = document.querySelector('.error-message');
+
+form.addEventListener('submit', (e) => {
+    const emailValue = document.querySelector('#email');
+
+    e.preventDefault();
+
+    if(emailValue.value === '' || emailValue.value == null) {
+        errorMsg.textContent = "Email address is blank.";
+        showError();
+        console.log(errorMsg.textContent);
+    } else if (!checkEmail(emailValue.value)){
+        errorMsg.textContent = "Please provide a valid email address."
+        showError();
+        console.log(errorMsg.textContent);
+    } else {
+        console.log("Submitted")
+        removeError();
+        document.email.submit();
+    }
+
+
+    function checkEmail(input){
+        let regexMail = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+        return regexMail.test(input)
+    }
+
+    function showError(){
+        form.classList = "form error"
+    }
+
+    function removeError(){
+        form.classList = "form"
+    }
+});
